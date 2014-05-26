5436aa70e834744e1e3b60726ec89c49769a2999
issyl0 <isabell@issyl0.co.uk>
2014-05-26 14:05:27 +0100

    Use instance variables and render the search_results view
    - Not global variables and redirect to the search_results view. I was
      noticing a conflict in two browsers - a request in one browser for
      YRK and another for GLD, both were showing GLD.
    - Don't need a route for /search_results, as a user GETting it
      without passing it any data would error, and now we're using
      render, the user never sees the /search_results route in their
      browser URL bar.
    - Splitting the station_name parameter on the ( to get only the first
      portion, the station name, not the abbreviation, left a trailing
      space. The SQL handled this, but it was annoying to see, so strip
      the trailing spaces.

--- application.rb
+++ application.rb
@@ -11,23 +11,23 @@ helpers do
   end
 
   def scrape_national_rail(station_abbr,dep=nil)
-    $selected_station = station_abbr
+    @selected_station = station_abbr
     nr_base_url = "http://ojp.nationalrail.co.uk"
     nr_board = "service/ldbboard"
-    $stopping_stations = []
+    @stopping_stations = []
 
     nr = Nokogiri::HTML.parse(open("#{nr_base_url}/#{nr_board}/#{dep ? 'dep' : 'arr'}/#{station_abbr}"))
     structure = "div.results.trains > div.tbl-cont > table > tbody > tr"
 
     # Get the end destination of the train.
     nr.css("#live-departure-board > #{structure} > td.destination").each do |destination|
-      $stopping_stations.push(destination.text.strip)
+      @stopping_stations.push(destination.text.strip)
       
       # Find the stations it stops at on its way to its destination.
       nr.css("#live-departure-board > #{structure} > td > a").each do |detail_url|
         du = Nokogiri::HTML.parse(open("#{nr_base_url}#{detail_url['href']}"))
         du.css("#live-departure-details > #{structure} > td.station").each do |stopping_station|
-          $stopping_stations.push(stopping_station.text.strip)
+          @stopping_stations.push(stopping_station.text.strip)
         end
       end
     end
@@ -40,16 +40,12 @@ end
 
 post '/search' do
   # Check that the requested station exists.
-  if station_abbr = find_station(params[:station_name].split('(')[0]).abbr
+  if station_abbr = find_station(params[:station_name].split('(')[0].strip).abbr
     if params[:arrival_station] == 'on'
       scrape_national_rail(station_abbr)
     elsif params[:departure_station] == 'on'
       scrape_national_rail(station_abbr,'dep')
     end
   end
-  redirect :'/search_results'  
-end
-
-get '/search_results' do
   erb :search_results
 end
